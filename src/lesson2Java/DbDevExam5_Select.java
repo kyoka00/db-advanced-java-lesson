3907d1754b8f5d4b28c0170f17c06450fa4c6a29
kyoka <k.maeno@ks-japan.jp>
2022-05-17 22:18:15 +0900

    発展問題終了

--- src/lesson2Java/DbDevExam5_Select.java
+++ src/lesson2Java/DbDevExam5_Select.java
@@ -2,8 +2,17 @@ package lesson2Java;
 
 import java.sql.Connection;
 
+
 public class DbDevExam5_Select {
-	private Connection connection;
+	public static void main(String [] args){
+		Connection connection;
+		ProductDao productDao;
+
+		connection = DbUtil.getConnection();
+		productDao = new ProductDao(connection);
+		Product result = productDao.findByProductId(102);
+		System.out.println("商品番号：" + result.getProductId() + ", 商品名：" + result.getProductName() + ", 値段：" + result.getPrice());
+	}
 	
 	
 	
